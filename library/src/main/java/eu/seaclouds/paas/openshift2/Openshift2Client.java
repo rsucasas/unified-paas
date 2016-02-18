ee9c7f7da877fa79dc0513eb616df3abd0963556
a572832 <roi.suicasas.external@atos.net>
2016-02-18 14:27:45 +0100

    Openshift client classes added

---
+++ library/src/main/java/eu/seaclouds/paas/openshift2/Openshift2Client.java
@@ -0,0 +1,27 @@
+package eu.seaclouds.paas.openshift2;
+
+import eu.seaclouds.paas.Credentials;
+import eu.seaclouds.paas.PaasClient;
+import eu.seaclouds.paas.PaasException;
+import eu.seaclouds.paas.PaasSession;
+
+
+/**
+ * 
+ *
+ * @author ATOS
+ * @date 18/2/2016-14:26:45
+ */
+public class Openshift2Client implements PaasClient
+{
+
+	
+	@Override
+	public PaasSession getSession(Credentials credentials) throws PaasException
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
---
+++ library/src/main/java/eu/seaclouds/paas/openshift3/Openshift3Client.java
@@ -0,0 +1,27 @@
+package eu.seaclouds.paas.openshift3;
+
+import eu.seaclouds.paas.Credentials;
+import eu.seaclouds.paas.PaasClient;
+import eu.seaclouds.paas.PaasException;
+import eu.seaclouds.paas.PaasSession;
+
+
+/**
+ * 
+ *
+ * @author ATOS
+ * @date 18/2/2016-14:26:32
+ */
+public class Openshift3Client implements PaasClient
+{
+	
+
+	@Override
+	public PaasSession getSession(Credentials credentials) throws PaasException
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
